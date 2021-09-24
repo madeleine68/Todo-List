d03d092c4cebcf28368d21a4ffac6cf3b14d53a7
Madeleine <46178706+madeleine68@users.noreply.github.com>
2021-09-24 18:13:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function newTask(title, description) {
+  const task = {
+    title : title,
+    description : description,
+    complete : false,
+
+    logState : function() {
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`)
+    },
+
+    markCompleted : function() {
+      this.complete = true;
+    }
+  }; 
+  return task
+}
+
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the ... out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "suprised"); // task 1
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
